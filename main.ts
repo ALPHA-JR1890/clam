4ae890f566cf8a64e81bb658736e9e442a5e9d21
JR <blackerfamily1@outlook.com>
2025-03-12 10:06:33 -0700

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,9 @@
+hummingbird.startHummingbird()
 basic.forever(function () {
-	
+    if (hummingbird.getSensor(SensorType.Distance, ThreePort.One) < 5) {
+        hummingbird.setPositionServo(FourPort.One, 90)
+        basic.pause(1000)
+    } else {
+        hummingbird.setPositionServo(FourPort.One, 0)
+    }
 })
